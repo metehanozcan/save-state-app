d27e0f36859e9d18783b1586daf7aa63743c5dca
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-09 03:12:32 +0300

    Saving state implementation

--- app/src/main/java/com/patika/week3/HomeFragment.kt
+++ app/src/main/java/com/patika/week3/HomeFragment.kt
@@ -11,20 +11,20 @@ import com.patika.week3.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-
+    private lateinit var tester: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.v("PATIKADEV", "onCreateView called.")//
+        Log.v("Test", "onCreateView called.")
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
-        //        return inflater.inflate(R.layout.fragment_home, container,false )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.v("Test", "onViewCreated called.")//
         binding.button2.setOnClickListener {
             binding.textView.text = binding.userInputText.text.toString()
         }
@@ -33,11 +33,10 @@ class HomeFragment : Fragment() {
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
         if (savedInstanceState != null) {
-            Log.v("Test", "Another Test")
+            Log.v("Test", "onViewStateRestored")
             binding.textView.text = savedInstanceState.getString("USER_TEXT", "DEFAULT VAL")
         }
     }
-
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
